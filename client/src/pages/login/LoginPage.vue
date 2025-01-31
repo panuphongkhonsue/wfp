6d762921def7b54f5580db61ee58887b637fa79b
SutthapatB <sutthapat.b@gmail.com>
2025-02-01 03:39:35 +0700

    Update LoginPage.vue

--- client/src/pages/login/LoginPage.vue
+++ client/src/pages/login/LoginPage.vue
@@ -1,76 +1,69 @@
 <template>
-    <div class="row bg-blue-10" style="height: 100vh;">
-      <div class="col-0 col-md-6 flex justify-center content-center">
-        <img src="../../assets/loginimage.svg" alt="login-image" class="responsive" />
-      </div>
+  <div class="row bg-blue-10 justify-center" style="height: 100vh;">
       <div class="col-12 col-md-6 flex justify-center content-center">
-        <q-card class="q-pa-lg shadow-2" :style="{ width: $q.screen.lt.sm ? '90%' : '60%' }">
-          <q-card-section class="flex justify-center">
-            <img src="../../assets/loginlogo.svg" alt="login-logo" />
-          </q-card-section>
-  
-          <q-card-section class="text-center">
-            <h2 class="text-h4 font-bold text-uppercase">ระบบเบิกสวัสดิการ</h2>
-          </q-card-section>
-          
-          <q-card-section>
-            <q-form @submit.prevent="login" class="q-gutter-md">
-              <!-- Username -->
-              <q-input 
-                v-model="username" 
-                label="Username" 
-                :rules="[val => !!val || 'กรุณากรอก Username']"
-              />s
-              <!-- Password -->
-              <q-input 
-                v-model="password" 
-                label="Password" 
-                type="password" 
-                :rules="[val => !!val || 'กรุณากรอก Password']"
-              />
-  
-              <!-- Remember Me -->
-              <q-checkbox v-model="remember" label="Remember Me" />
-              <!-- Login Button -->
-              <q-btn class="full-width" type="submit" color="primary" label="Login" />
-            </q-form>
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
-  </template>
-  
-  <script setup>
-  import { ref, onMounted } from 'vue';
+          <q-card class="q-pa-md " :style="{
+              width: $q.screen.lt.sm ? '90%' : '60%',
 
-  const username = ref('');
-  const password = ref('');
-  const remember = ref(false);
-  
-  // โหลด Username จาก localStorage 
-  onMounted(() => {
-    const savedUsername = localStorage.getItem('rememberedUsername');
-    if (savedUsername) {
+              borderRadius: '12px',
+              boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.7)'
+          }">
+              <q-card-section class="flex justify-center">
+                  <img src="../../assets/loginlogo.svg" alt="login-logo" />
+              </q-card-section>
+
+              <q-card-section class="text-center">
+                  <h2 class="text-h4 font-bold text-uppercase">ระบบเบิกสวัสดิการ</h2>
+              </q-card-section>
+
+              <q-card-section>
+                  <q-form @submit="login" class="q-gutter-md q-px-xl">
+                      <!-- Username -->
+                      <q-input v-model="username" label="Username" :rules="[val => !!val || 'กรุณากรอก Username']" />
+                      <!-- Password -->
+                      <q-input v-model="password" label="Password" type="password"
+                          :rules="[val => !!val || 'กรุณากรอก Password']" />
+                      <!-- Remember Me -->
+                      <q-checkbox size="sm" v-model="remember" label="Remember Me" />
+                      <!-- Login Button -->
+                      <q-btn class="full-width" type="submit" color="primary" label="Login" />
+                  </q-form>
+              </q-card-section>
+          </q-card>
+      </div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const username = ref('');
+const password = ref('');
+const remember = ref(false);
+
+// โหลด Username จาก localStorage 
+onMounted(() => {
+  const savedUsername = localStorage.getItem('rememberedUsername');
+  if (savedUsername) {
       username.value = savedUsername;
       remember.value = true;
-    }
-  });
-  
-  // Login
-  const login = () => {
-    if (!username.value || !password.value) {
+  }
+});
+
+// Login
+const login = () => {
+  if (!username.value || !password.value) {
       console.log('กรุณากรอก Username และ Password');
       return;
-    }
-  
-    console.log('เข้าสู่ระบบด้วย', username.value, password.value);
-  
-    if (remember.value) {
+  }
+
+  console.log('เข้าสู่ระบบด้วย', username.value, password.value);
+
+  if (remember.value) {
       localStorage.setItem('rememberedUsername', username.value);
-    } else {
+  } else {
       localStorage.removeItem('rememberedUsername');
-    }
-  
-  };
-  </script>
-  
+  }
+
+};
+</script>
