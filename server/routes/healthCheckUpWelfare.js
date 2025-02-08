6fb4365421a7f5eadf5c924aa016fdd54e9760b1
Panuphong Khonsue <118897428+panuphongkhonsue@users.noreply.github.com>
2025-02-08 18:10:28 +0700

    implement : update backend for health check up welfare

--- server/routes/healthCheckUpWelfare.js
+++ server/routes/healthCheckUpWelfare.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const reimbursementsGeneralController = require('../controllers/healthCheckUpWelfareController');
-const { authPermission, bindFilter, getRemaining, checkRemaining } = require('../middleware/healthCheckUpWelfare')
+const { authPermission, bindFilter, getRemaining, checkRemaining, bindUpdate, bindCreate, deletedMiddleware } = require('../middleware/healthCheckUpWelfare')
 router.get('/', authPermission, bindFilter, reimbursementsGeneralController.list);
 router.get('/remaining', authPermission, getRemaining, reimbursementsGeneralController.getRemaining);
 router.get('/:id', authPermission, reimbursementsGeneralController.getById);
-router.post('/', authPermission, getRemaining, checkRemaining, reimbursementsGeneralController.create);
-// router.put('/:id', authPermission, reimbursementsGeneralController.update);
-// router.delete('/:id', authPermission, reimbursementsGeneralController.delete);
+router.post('/', authPermission, bindCreate, getRemaining, checkRemaining, reimbursementsGeneralController.create);
+router.put('/:id', authPermission, bindUpdate, getRemaining, checkRemaining, reimbursementsGeneralController.update);
+router.delete('/:id', authPermission, deletedMiddleware, reimbursementsGeneralController.delete);
 module.exports = router;
