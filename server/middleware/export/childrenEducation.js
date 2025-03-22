2ac78e7cb6cf156541acbcae90b0d776c09e633a
phurin284 <119477542+phurin284@users.noreply.github.com>
2025-03-22 19:22:52 +0700

    implement
    me export file childrenEducation

---
+++ server/middleware/export/childrenEducation.js
@@ -0,0 +1,196 @@
+const { initLogger } = require('../../logger');
+const logger = initLogger('ExportChildrenEduCationFetchData');
+const { Op, col } = require('sequelize');
+const statusText = require('../../enum/statusText');
+const category = require('../../enum/category');
+const status = require('../../enum/status');
+const { formatDateThaiSlash } = require('../../enum/formatDate');
+const { reimbursementsChildrenEducation,reimbursementsChildrenEducationHasChildrenInfomation,childrenInfomation, users, positions, categories, sector, employeeTypes, departments, subCategories, sequelize } = require('../../models/mariadb');
+const { getFiscalYearDynamic, getFiscalYear } = require('../../middleware/utility');
+
+const fetchDatareimChildrenEducation = async (req, res, next) => {
+    const method = 'FetchHealthCheckupData';
+    const { id } = req.user;
+    const dataId = req.params['id'];
+    try {
+        const requestData = await reimbursementsChildrenEducation.findOne({
+            attributes: [
+                'id',
+                [col("reim_number"), "reimNumber"],
+                [col("fund_sum_request"), "fundSumRequest"],
+                [col('marry_regis'), "marryRegis"],
+                [col('fund_other'), "fundOther"],
+                'eligible',
+                'spouse',
+                'status',
+                'role',
+                [col("position"), "positionSpouse"],
+                [col("department"), "departmentSpouse"],
+                [col('parental_status'), "parentalStatus"],
+                [col('eligible_benefits'), "eligibleBenefits"],
+                [col('eligible_sub_benefits'), "eligibleSubSenefits"],
+                [col("created_by_user.id"), "userId"],
+                [col("created_by_user.name"), "name"],
+                [col("created_by_user.position.id"), "positionId"],
+                [col("created_by_user.position.name"), "position"],
+                [col("created_by_user.employee_type.id"), "employeeTypeId"],
+                [col("created_by_user.employee_type.name"), "employeeType"],
+                [col("created_by_user.sector.name"), "sector"],
+                [col("created_by_user.department.name"), "department"],
+                [col("created_by_user.house_number"), "houseNumber"],
+                [col("created_by_user.street"), "street"],
+                [col("created_by_user.district"), "district"],
+                [col("created_by_user.province"), "province"],
+                [col("created_by_user.sub_district"), "subDistrict"],
+                [col("created_by_user.postal_code"), "postalCode"],
+                [col("created_by_user.first_working_date"), "firstWorkingDate"],
+                [col("category.id"), "categoryId"],
+                [col("category.name"), "categoryName"],
+            ],
+            include: [
+                {
+                    model: users, as: 'created_by_user',
+                    attributes: [],
+                    include: [
+                        {
+                            model: positions, as: 'position',
+                            attributes: []
+                        },
+                        {
+                            model: employeeTypes, as: 'employee_type',
+                            attributes: []
+                        },
+                        {
+                            model: sector, as: 'sector',
+                            attributes: []
+                        },
+                        {
+                            model: departments, as: 'department',
+                            attributes: []
+                        },
+                    ]
+                },
+                {
+                    model: categories,
+                    as: "category",
+                    attributes: []
+                }
+            ],
+            where: { id: dataId, status: statusText.waitApprove},
+        });
+        const childrenData = await childrenInfomation.findAll({
+            attributes: [
+                'id',
+                [col('childrenInfomation.fund_receipt'), "fundReceipt"],
+                [col('childrenInfomation.fund_other'), "fundOther"],
+                [col('childrenInfomation.fund_sum_request'), "fundSumRequest"],
+                [col('childrenInfomation.child_name'), "childName"],
+                [col('childrenInfomation.child_number'), "childNumber"],
+                [col('childrenInfomation.child_type'), "childType"],
+                [col('childrenInfomation.child_birth_day'), "childBirthDay"],
+                [col('childrenInfomation.fund_university'), "fundUniversity"],
+                [col('childrenInfomation.fund_sub_university'), "fundSubUniversity"],
+                [col('childrenInfomation.child_father_number'), "childFatherNumber"],
+                [col('childrenInfomation.child_mother_number'), "childMotherNumber"],
+                [col('childrenInfomation.child_type'), "childType"],
+                [col('childrenInfomation.school_name'), "schoolName"],
+                [col('childrenInfomation.school_type'), "schoolType"],
+                [col('delegate_name'), "delegateName"],
+                [col('delegate_number'), "delegateNumber"],
+                [col('delegate_birth_day'), "delegateBirthDay"],
+                [col('delegate_death_day'), "delegateDeathDay"],
+                'district',
+                'province',
+                [col('sub_category.name'), "SubCategoryName"],
+            ],
+            include: [
+                {
+                    model: subCategories,
+                    as: "sub_category",
+                    attributes: ['id', 'name']
+                },
+                {
+                    model: reimbursementsChildrenEducationHasChildrenInfomation,
+                    as: "reimbursements_children_education_has_children_infomations",
+                    required: true,
+                    attributes: [],
+                    where: { reimbursements_children_education_id: dataId },
+                    include: [
+                        {
+                            model: reimbursementsChildrenEducation,
+                            as: "reimbursements_children_education",
+                            required: true,
+                            attributes: [],
+                            where: {
+                                created_by: id
+                            }
+                        }
+                    ]
+                }
+            ],
+        });
+        if (!requestData && !childrenData) {
+            logger.info('Data not Found', { method, data: { id } });
+            return res.status(200).json({
+                message: "ไม่พบข้อมูล"
+            });
+        }
+        const datas = JSON.parse(JSON.stringify(requestData));
+        console.log("Children Data:", datas);
+        const child = childrenData.map(c => c.get({ plain: true })); 
+        var welfareData = {
+            ...datas,
+            user: {
+                employeeTypeId: datas.employeeTypeId,
+                userId: datas.userId,
+                name: datas.name,
+                position: datas.position,
+                employeeType: datas.employeeType,
+                sector: datas.sector,
+                department: datas.department,
+                houseNumber: datas.houseNumber,
+                subDistrict: datas.subDistrict,
+                postalCode: datas.postalCode,
+                street: datas.street,
+                district: datas.district,
+                province: datas.province,
+                firstWorkingDate: datas.firstWorkingDate
+
+            },
+            children: child,
+            receiptInfo: child.map(c => ({
+                categoryName: c.childName,
+                fundSumRequest: c.fundSumRequest,
+            })),
+            
+            total: datas.fundSumRequest,
+        }
+        delete welfareData.categoryName;
+        delete welfareData.userId;
+        delete welfareData.name;
+        delete welfareData.position;
+        delete welfareData.employeeType;
+        delete welfareData.sector;
+        delete welfareData.department;
+        delete welfareData.employeeTypeId;
+        delete welfareData.houseNumber;
+        delete welfareData.subDistrict;
+        delete welfareData.postalCode;
+        delete welfareData.street;
+        delete welfareData.district;
+        delete welfareData.province;
+        req.body.datas = welfareData;
+        next();
+    }
+    catch (error) {
+        logger.error(`Error ${error.message}`, {
+            method,
+            data: { id },
+        });
+        next(error);
+    }
+};
+
+module.exports = {
+    fetchDatareimChildrenEducation,
+};
--- server/routes/export.js
+++ server/routes/export.js
@@ -6,6 +6,8 @@ const { createPdfAssist } = require('../controllers/export/assist');
 const { fetchDataVarious, fetchDataFuneralFamily } = require('../middleware/export/assist');
 const { createPdfFuneralDeceaseEmployee } = require('../controllers/export/funeralDeceaseEmployee');
 const { fetchDataFuneralDeceaseEmployee } = require('../middleware/export/funeralDeceaseEmployee');
+const { createPdfChildrenEducation } = require('../controllers/export/childrenEducation');
+const { fetchDatareimChildrenEducation } = require('../middleware/export/childrenEducation');
 router.get('/health-check-up/:id', fetchDataHealthCheckup, createPdfGeneral);
 
 router.get('/dental/:id', fetchDataDental, createPdfGeneral);
@@ -17,4 +19,6 @@ router.get('/various/:id', fetchDataVarious, createPdfAssist);
 router.get('/various-Funeral-Family/:id', fetchDataFuneralFamily, createPdfAssist);
 
 router.get('/funeral-Decease-Employee/:id', fetchDataFuneralDeceaseEmployee, createPdfFuneralDeceaseEmployee);
+
+router.get('/Children-Education/:id', fetchDatareimChildrenEducation, createPdfChildrenEducation);
 module.exports = router;
